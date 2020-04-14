f237cc89812c39ff331743f35ee0fe603a26ca3b
zachbrown12 <zachmb22@gmail.com>
2020-04-14 14:38:46 -0400

    Added external API

--- Backend/db/migrate/20200413181908_create_doctors.rb
+++ Backend/db/migrate/20200413181908_create_doctors.rb
@@ -2,9 +2,11 @@ class CreateDoctors < ActiveRecord::Migration[6.0]
   def change
     create_table :doctors do |t|
       t.string :name
-      t.string :location
+      t.string :city
+      t.string :state
       t.string :hospital
       t.string :practice
+      t.string :phone_number
       t.float :participation
       t.text :notes
 
