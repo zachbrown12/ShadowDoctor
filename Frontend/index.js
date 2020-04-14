d140faeea215f39249bf4131fee0fb3baad58e09
komald4 <komald.4@gmail.com>
2020-04-14 14:42:16 -0400

    js html css

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,16 +1,209 @@
 document.addEventListener("DOMContentLoaded", () => {
+    fetchStudents();
+  
+  });
+  
+  //event listeners
+  
+  const menuListener = () =>
+    document.querySelector(".menu").addEventListener("click", e => {
+      if (e.target.className === "button") {
+        printOrder(e.target.dataset);
+      }
+    });
+  
+  const formListener = () => {
+    const burgerForm = document.getElementById("custom-burger");
+    burgerForm.addEventListener("submit", e => {
+      e.preventDefault();
+      const burgerObj = formToObject(burgerForm);
+      const config = postConfig(burgerObj);
+      postBurger(config);
+      burgerForm.reset();
+    });
+  };
+  
+  //api calls
+    function fetchStudents(){
+        fetch("http://localhost:3000/students") 
+            .then( response => response.json())
+            .then(students => showStudents(students))
+    }
 
-fetchStudents()
+    function showStudents(students){
+        console.log(students)
+    }
+  
+  const renderBurgers = burgers =>
+    burgers.forEach(burger => {
+      document.querySelector("#burger-menu").innerHTML += printBurger(burger);
+    });
+  
+  const postBurger = config =>
+    fetch(BURGER_URL, config)
+      .then(r => r.json())
+      .then(json => renderPost(json));
+  
+  const renderPost = burger => {
+    printOrder(burger);
+    document.querySelector("#burger-menu").innerHTML += printBurger(burger);
+  };
+  
+  //template
+  
+  const printBurger = burger => `<div class="burger">
+  <h3 class="burger_title">${burger.name}</h3>
+    <img src="${burger.image}">
+    <p class="burger_description">
+      ${burger.description}
+    </p>
+    <button data-name=${burger.name} class="button">Add to Order</button>
+  </div>`;
+  
+  const orderTemplate = burger =>
+    `<li id="${burger.name}">${burger.name} burger  1`;
+  
+  const printOrder = burger => {
+    const orders = document.querySelector(".order");
+    const existingItem = document.getElementById(`${burger.name}`);
+    if (existingItem === null) {
+      orders.innerHTML += orderTemplate(burger);
+    } else {
+      const currentNum =
+        parseInt(existingItem.innerText[existingItem.innerText.length - 1]) + 1;
+  
+      existingItem.innerText = `${existingItem.id} burger ${currentNum}`;
+    }
+  };
+  
+  //config
+  
+  const postConfig = obj => ({
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify(obj)
+  });
+  
+  //other
+  const formToObject = form => {
+    const [name, description, image] = [...form.elements]
+      .filter(i => i.type != "submit")
+      .map(i => i.value);
+    return { name, description, image };
+  };
 
 
-function fetchStudents(){
-    fetch("http://localhost:3000/students") 
-        .then( response => response.json())
-        .then(students => showStudents(students))
-}
+// document.addEventListener("DOMContentLoaded", () => {
 
-function showStudents(students){
-    console.log(students)
-}
-})
+// // fetchStudents()
+// getBurgers();
+// menuListener();
+// formListener();
+
+// // function fetchStudents(){
+// //     fetch("http://localhost:3000/students") 
+// //         .then( response => response.json())
+// //         .then(students => showStudents(students))
+// // }
+
+// // function showStudents(students){
+// //     console.log(students)
+// // }
+
+
+  
+  
+//   //event listeners
+  
+//   const menuListener = () =>
+//     document.querySelector(".menu").addEventListener("click", e => {
+//       if (e.target.className === "button") {
+//         printOrder(e.target.dataset);
+//       }
+//     });
+  
+//   const formListener = () => {
+//     const burgerForm = document.getElementById("custom-burger");
+//     burgerForm.addEventListener("submit", e => {
+//       e.preventDefault();
+//       const burgerObj = formToObject(burgerForm);
+//       const config = postConfig(burgerObj);
+//       postBurger(config);
+//       burgerForm.reset();
+//     });
+//   };
+  
+//   //api calls
+//   const BURGER_URL = "http://localhost:3000/burgers";
+  
+//   const getBurgers = () => {
+//     fetch(BURGER_URL)
+//       .then(r => r.json())
+//       .then(json => renderBurgers(json));
+//   };
+  
+//   const renderBurgers = burgers =>
+//     burgers.forEach(burger => {
+//       document.querySelector("#burger-menu").innerHTML += printBurger(burger);
+//     });
+  
+//   const postBurger = config =>
+//     fetch(BURGER_URL, config)
+//       .then(r => r.json())
+//       .then(json => renderPost(json));
+  
+//   const renderPost = burger => {
+//     printOrder(burger);
+//     document.querySelector("#burger-menu").innerHTML += printBurger(burger);
+//   };
+  
+//   //template
+  
+//   const printBurger = burger => `<div class="burger">
+//   <h3 class="burger_title">${burger.name}</h3>
+//     <img src="${burger.image}">
+//     <p class="burger_description">
+//       ${burger.description}
+//     </p>
+//     <button data-name=${burger.name} class="button">Add to Order</button>
+//   </div>`;
+  
+//   const orderTemplate = burger =>
+//     `<li id="${burger.name}">${burger.name} burger  1`;
+  
+//   const printOrder = burger => {
+//     const orders = document.querySelector(".order");
+//     const existingItem = document.getElementById(`${burger.name}`);
+//     if (existingItem === null) {
+//       orders.innerHTML += orderTemplate(burger);
+//     } else {
+//       const currentNum =
+//         parseInt(existingItem.innerText[existingItem.innerText.length - 1]) + 1;
+  
+//       existingItem.innerText = `${existingItem.id} burger ${currentNum}`;
+//     }
+//   };
+  
+//   //config
+  
+//   const postConfig = obj => ({
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json"
+//     },
+//     body: JSON.stringify(obj)
+//   });
+  
+//   //other
+//   const formToObject = form => {
+//     const [name, description, image] = [...form.elements]
+//       .filter(i => i.type != "submit")
+//       .map(i => i.value);
+//     return { name, description, image };
+//   };
+// })
 
