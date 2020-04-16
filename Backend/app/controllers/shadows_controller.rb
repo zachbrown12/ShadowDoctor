5e7b4ec57d247970043e93e63bb8e7ae862a62b5
zachbrown12 <zachmb22@gmail.com>
2020-04-16 12:00:14 -0400

    Added Shadows Logic

--- Backend/app/controllers/shadows_controller.rb
+++ Backend/app/controllers/shadows_controller.rb
@@ -1,2 +1,24 @@
 class ShadowsController < ApplicationController
+
+    def create
+        shadow = Shadow.create(shadow_params)
+        render json: shadow, except: [:updated_at, :created_at]
+    end
+
+    def index
+        shadows = Shadow.all
+        render :json => shadows, :include => ['student', 'doctor']
+    end
+
+
+
+
+
+
+    private
+
+    def shadow_params
+        params.require(:shadow).permit(:student_id, :doctor_id, :start_date, :length, :accepted)
+    end
+
 end
